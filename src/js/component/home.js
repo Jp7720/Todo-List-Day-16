0863dc63cb089f4b3b96fb285e1053b3ed1ec1c6
jeffry porras <jeffryporrasbarahona@gmail.com>
2021-04-05 00:09:26 +0000

    simple

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,63 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
 
 //create your first component
 export function Home() {
+	const [tareas, setTareas] = useState("");
+	const [lista, setLista] = useState([]);
+
+	// const Itemdelete = index => {
+	// 	let updatedlista = lista.filter(
+	// 		(tareas, tareasIndex) => index != tareasIndex
+	// 	);
+	// 	setLista(updatedlista);
+	// };
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="App-content">
+			<div className="  text-center mt-5">
+				<h1>TO DO LIST </h1>
+				<input
+					className="todoInput "
+					type="text"
+					autoFocus={true}
+					placeholder="Ingrese sus datos"
+					onChange={e => setTareas(e.target.value)}
+					value={tareas}
+					onKeyPress={e => {
+						if (e.key == "Enter") {
+							setLista(lista.concat(tareas));
+							setTareas("");
+							console.log({ lista });
+						}
+					}}
+				/>
+
+				<ul className="list-group">
+					{!lista
+						? "loading..."
+						: lista.map((item, index) => {
+								return (
+									<label
+										className="form-control text-success list-group-item list-group-item-action"
+										key={index}
+										onDoubleClick={() => {
+											setLista(
+												lista.filter(
+													(itemf, indexf) =>
+														indexf !== index
+												)
+											);
+										}}>
+										{item}
+									</label>
+								);
+						  })}
+				</ul>
+
+				<p className="text-muted card pl-1">
+					{lista.length} items left
+				</p>
+			</div>
 		</div>
 	);
 }
